ca2d74b196331bd1dcb901e6d07316e39f93c410
LEFRANCOIS Maxime <maxime.lefrancois.86@gmail.com>
2016-09-07 14:31:26 +0200

    first draft

---
+++ src/main/java/com/github/andreiciortea/stn/website/App.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2016 Ecole des Mines de Saint-Etienne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.andreiciortea.stn.website;
+
+import com.github.thesmartenergy.rdfp.BaseURI;
+import javax.enterprise.inject.Produces;
+
+public class App {
+    
+    @Produces
+    @BaseURI
+    public String getBase() { 
+        return "https://w3id.org/stn/";
+    }
+}
---
+++ src/site/markdown/index.md
@@ -0,0 +1,11 @@
+# The STN Website
+
+This website exposes the SNT ontologies.
+
+See:
+
+- [The core ontology](core)
+- [The core/operations ontology](core/operations)
+- [The core/operations/http ontology](core/operations/http)
+- [The code of this web application.](https://github.com/andreiciortea/stn-website)
+
